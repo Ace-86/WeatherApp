487f4aba051429af9e170c73cfea43880e293f56
Ace <kronicrespawn@gmail.com>
2022-10-06 21:43:26 -0500

    5 day forecast functioning; not linked to geocode

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ const cityLoc = document.querySelector('#location');
 const currentTempature = document.querySelector('#curr_temp');
 const weatherDescription = document.querySelector('#desc_weather');
 const windSpeed = document.querySelector('#wind_speed');
-var longitude;
-var latitude;
+
 // -------------geocode------------
 //on click, find location of city
 //city will bring up a list in array, having a different state
 //match populated state with the users state input
 //once matched, take the longitude and latitude numbers, save as a variable, and input them into weather makeRequest function
-    async function getGeocode() {
+async function getGeoCode() {
     try {
     const user_state = userState.value.charAt(0).toUpperCase() + userState.value.slice(1).toLowerCase();
     const user_city = userCity.value.charAt(0).toUpperCase() + userCity.value.slice(1).toLowerCase();
@@ -28,49 +27,49 @@ var latitude;
     const arrGeo = getGeo.filter(arrGeo => arrGeo.state.indexOf(`${user_state}`) !== -1);
     console.log(arrGeo);
 
-    //get lon and lat from arrGeo
+//get lon and lat from arrGeo
     longitude = arrGeo[0].lon;
     latitude = arrGeo[0].lat;
     console.log(longitude);
     console.log(latitude);
     
-    return {lon : longitude, lat: latitude}
-     } catch (err) {
+    return {lon : longitude, lat : latitude}
+    } catch (err) {
         console.log('geo code error')
     } 
 }
 
 async function makeRequest() {
     try { 
-        const response2 = await fetch('api.openweathermap.org/data/2.5/forecast?lat={'+`${latitude.value}`+'}&lon={' + `${longitude}` + '}&appid=48f9e697bfab037187eccf9b6153b9e9', {
+        const responseTwo = await fetch('http://api.openweathermap.org/data/2.5/forecast?lat=41.8755616&lon=-87.6244212&appid=48f9e697bfab037187eccf9b6153b9e9', {
         mode: 'cors'
     })
-    const weatherData = await response2.json();
+    const weatherData = await responseTwo.json();
         console.log(weatherData)
-        console.log(weatherData.main.temp)
-        console.log(weatherData.name)
-        console.log(weatherData.sys.country)
-        console.log(weatherData.weather[0].description)
-        console.log(weatherData.wind)
+        // console.log(weatherData.main.temp)
+        // console.log(weatherData.name)
+        // console.log(weatherData.sys.country)
+        // console.log(weatherData.weather[0].description)
+        // console.log(weatherData.wind)
 
-        const wind = JSON.stringify(weatherData.wind)
-        const country = JSON.stringify(weatherData.sys.country)
-        const location = JSON.stringify(weatherData.name)
-        const description = JSON.stringify(weatherData.weather[0].description)
-        const weather = JSON.stringify(weatherData.main.temp);
+        // const wind = JSON.stringify(weatherData.wind)
+        // const country = JSON.stringify(weatherData.sys.country)
+        // const location = JSON.stringify(weatherData.name)
+        // const description = JSON.stringify(weatherData.weather[0].description)
+        // const weather = JSON.stringify(weatherData.main.temp);
         
-        cityLoc.innerHTML = location + ',' + country;
-        currentTempature.innerHTML = "The current temperature is " + weather + ' F';
-        weatherDescription.innerHTML = "Today's Forecast: " + description;
-        windSpeed.innerHTML = "Wind Speed: " + wind
+        // cityLoc.innerHTML = location + ',' + country;
+        // currentTempature.innerHTML = "The current temperature is " + weather + ' F';
+        // weatherDescription.innerHTML = "Today's Forecast: " + description;
+        // windSpeed.innerHTML = "Wind Speed: " + wind
     } catch (err) {
             console.log('error')
     }
 }
 
+makeRequest()
 
 
 submission.addEventListener('click', () => {
-    var data = getGeocode();
-    console.log('this is' + data)
+    getGeoCode();
     })
