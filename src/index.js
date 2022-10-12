26ec08015d67817f18472cd58d131057c552df88
Ace <kronicrespawn@gmail.com>
2022-10-11 19:57:19 -0500

    date and times added to display

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ async function getGeoCode() {
 
         const currWeather = await currForecast.json();
         const weatherData = await dailyForecast.json();
+        console.log(weatherData);
         var user_State = arrGeo[0].state;
         var user_City = arrGeo[0].name;
 
@@ -36,31 +37,37 @@ async function getGeoCode() {
         document.getElementById("weather_icon").src = "../src/images/" + iconcode + ".png";
 
         //next day, day 1 forecast
-        const description1 = JSON.stringify(weatherData.list[8].weather[0].description)
-        const weather1 = JSON.stringify(weatherData.list[8].main.temp);
-        const iconcode2 =  weatherData.list[8].weather[0].icon;
+        const description1 = JSON.stringify(weatherData.list[4].weather[0].description)
+        const weather1 = JSON.stringify(weatherData.list[4].main.temp);
+        const iconcode2 =  weatherData.list[4].weather[0].icon;
+        const nextDate1 =  weatherData.list[4].dt_txt;
         cityLoc1.innerHTML = user_City + ', ' + user_State;
         currentTempature1.innerHTML = "Temperature: " + weather1 + ' F';
         weatherDescription1.innerHTML = "Forecast: " + description1;
         document.getElementById("weather_icon2").src = "../src/images/" + iconcode2 + ".png";
+        future_date1.innerHTML = "Date: " + nextDate1;
         
         // day 2 forecast
-        const description2 = JSON.stringify(weatherData.list[16].weather[0].description)
-        const weather2 = JSON.stringify(weatherData.list[16].main.temp);
-        const iconcode3 =  weatherData.list[16].weather[0].icon;
+        const description2 = JSON.stringify(weatherData.list[12].weather[0].description)
+        const weather2 = JSON.stringify(weatherData.list[12].main.temp);
+        const iconcode3 =  weatherData.list[12].weather[0].icon;
+        const nextDate2 =  weatherData.list[12].dt_txt;
         cityLoc2.innerHTML = user_City + ', ' + user_State;
         currentTempature2.innerHTML = "Temperature: " + weather2 + ' F';
         weatherDescription2.innerHTML = "Forecast: " + description2;
         document.getElementById("weather_icon3").src = "../src/images/" + iconcode3 + ".png";
+        future_date2.innerHTML = "Date: " + nextDate2;
 
         // day 3 forecast
-        const description3 = JSON.stringify(weatherData.list[24].weather[0].description)
-        const weather3 = JSON.stringify(weatherData.list[24].main.temp);
-        const iconcode4 =  weatherData.list[24].weather[0].icon;
+        const description3 = JSON.stringify(weatherData.list[20].weather[0].description)
+        const weather3 = JSON.stringify(weatherData.list[20].main.temp);
+        const iconcode4 =  weatherData.list[20].weather[0].icon;
+        const nextDate3 =  weatherData.list[20].dt_txt;
         cityLoc3.innerHTML = user_City + ', ' + user_State;
         document.getElementById("weather_icon4").src = "../src/images/" + iconcode4 + ".png";
         currentTempature3.innerHTML = "Temperature: " + weather3 + ' F';
         weatherDescription3.innerHTML = "Forecast: " + description3;
+        future_date3.innerHTML = "Date: " + nextDate3;
 
         document.querySelector("#id_card").style.visibility = "visible";
         document.querySelector("#id_card1").style.visibility = "visible";
